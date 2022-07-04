471801b4bafc57abf4b78b5e0eccae6e8b92e2c8
Swayam Gupta <swayamgupta5705367@gmail.com>
2022-07-04 22:34:54 +0530

    production changes

--- client/build/static/js/13.5f2da71e.chunk.js
+++ client/build/static/js/13.5f2da71e.chunk.js
@@ -1,2 +1,1 @@
 (this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[13,12],{1302:function(n,c){},1310:function(n,c){},1323:function(n,c){}}]);
-//# sourceMappingURL=13.5f2da71e.chunk.js.map
--- client/build/static/js/15.9ead211b.chunk.js
+++ client/build/static/js/15.9ead211b.chunk.js
@@ -1,2 +1,1 @@
 (this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[15],{1446:function(n,c){},1448:function(n,c){}}]);
-//# sourceMappingURL=15.9ead211b.chunk.js.map
